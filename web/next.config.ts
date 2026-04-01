8f7a0ab7ae21b6a8eb4088cc26159302498945dd
ole <ohfb96@gmail.com>
2026-04-01 18:19:16 +0200

    fix: allow sanity cdn images in next image config

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+// Next.js config
+// Allows external images from the Sanity CDN
+
 const nextConfig: NextConfig = {
-  /* config options here */
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "cdn.sanity.io",
+			},
+		],
+	},
 };
 
 export default nextConfig;
